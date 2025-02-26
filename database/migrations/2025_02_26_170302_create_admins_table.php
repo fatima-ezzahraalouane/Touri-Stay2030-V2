bd4afb61f97f041763da9fe231921811133b1114
fatima-ezzahraalouane <falouane38@gmail.com>
2025-02-26 17:15:57 +0000

    T2-42 T2-43 T2-44 definir la table admins

--- database/migrations/2025_02_26_170302_create_admins_table.php
+++ database/migrations/2025_02_26_170302_create_admins_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,10 +12,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('admins', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        DB::statement("
+            CREATE TABLE admins (
+                CHECK (role_id = 1)
+            ) INHERITS (users);
+        ");
     }
 
     /**
