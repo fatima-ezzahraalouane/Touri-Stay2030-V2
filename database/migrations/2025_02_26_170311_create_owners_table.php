656a311408efd466b569642fda1c659d388eaaf3
fatima-ezzahraalouane <falouane38@gmail.com>
2025-02-26 17:18:28 +0000

    T2-42 T2-43 T2-44 definir la table owners

--- database/migrations/2025_02_26_170311_create_owners_table.php
+++ database/migrations/2025_02_26_170311_create_owners_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,10 +12,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('owners', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        DB::statement("
+            CREATE TABLE owners (
+                phone_number VARCHAR(20),
+                CHECK (role_id = 3)
+            ) INHERITS (users);
+        ");
     }
 
     /**
