4b436bd2731d0bca6d9b912f8fa0211ecdb7bea8
fatima-ezzahraalouane <falouane38@gmail.com>
2025-02-26 17:11:43 +0000

    T2-42 T2-43 T2-44 definir les propietes de la table roles

--- database/migrations/2025_02_26_170248_create_roles_table.php
+++ database/migrations/2025_02_26_170248_create_roles_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,8 +12,11 @@ return new class extends Migration
      */
     public function up(): void
     {
+        DB::statement("CREATE TYPE role_user AS ENUM ('Admin', 'Tourist', 'Owner')");
+
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
+            $table->enum('name_user', ['Admin', 'Tourist', 'Owner'])->unique();
             $table->timestamps();
         });
     }
@@ -23,5 +27,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('roles');
+        DB::statement("DROP TYPE role_user");
     }
 };
