5a5daef6ed8e25608e8a6c75645d10dc1be42f66
fatima-ezzahraalouane <falouane38@gmail.com>
2025-02-26 20:24:42 +0000

    T2-42 T2-43 T2-44 fixing table tourists

---
+++ database/migrations/2025_02_26_201627_create_tourists_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement("
+            CREATE TABLE tourists (
+                country VARCHAR(100),
+                CHECK (role_id = 2)
+            ) INHERITS (users);
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tourists');
+    }
+};
