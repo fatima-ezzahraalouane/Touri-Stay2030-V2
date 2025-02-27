9b8a815ab205f8681d82e1f9f8e0a5546fbcf473
fatima-ezzahraalouane <falouane38@gmail.com>
2025-02-27 10:55:47 +0000

    T2-7 T2-9 T2-2 T2-17 T2-18 creer controller AuthenticatedSessionController

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -9,27 +9,51 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use Illuminate\Validation\ValidationException;
 
 class AuthenticatedSessionController extends Controller
 {
     /**
      * Display the login view.
      */
-    public function create(): View
-    {
-        return view('auth.login');
-    }
+    // public function create(): View
+    // {
+    //     return view('auth.login');
+    // }
 
     /**
      * Handle an incoming authentication request.
      */
     public function store(LoginRequest $request): RedirectResponse
     {
-        $request->authenticate();
+        // $request->authenticate();
 
-        $request->session()->regenerate();
+        // $request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        // return redirect()->intended(RouteServiceProvider::HOME);
+
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            $user = Auth::user();
+
+            if ($user->role_id == 1) {
+                return redirect()->route('admin.dashboard');
+            } elseif ($user->role_id == 2) {
+                return redirect()->route('home');
+            } elseif ($user->role_id == 3) {
+                return redirect()->route('owner.dashbord');
+            }
+        }
+
+        throw ValidationException::withMessages([
+            'email' => __('auth.failed'),
+        ]);
     }
 
     /**
