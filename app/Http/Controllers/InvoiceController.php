9a4bfce5f3b0b05cb95e07e2e7215ab8df9019a7
fatima-ezzahraalouane <falouane38@gmail.com>
2025-03-09 17:36:06 +0000

    T2V-4 T2V-5 T2V-6 T2V-10 T2V-9 T2V-2 T2V-7 T2V-12 ajouter un contrôleur pour fpdf pour que le touriste puisse le télécharger

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -17,18 +17,18 @@ class InvoiceController extends Controller
         $pdf->AddPage();
         $pdf->SetFont('Arial', 'B', 16);
 
-        $pdf->Cell(190, 10, 'Facture de Réservation', 1, 1, 'C');
+        $pdf->Cell(190, 10, 'Facture de Reservation', 1, 1, 'C');
         $pdf->Ln(10);
 
         $pdf->SetFont('Arial', '', 12);
-        $pdf->Cell(190, 10, 'Réservation pour : ' . $reservation->user->name, 0, 1);
+        $pdf->Cell(190, 10, 'Reservation pour : ' . $reservation->user->name, 0, 1);
         $pdf->Cell(190, 10, 'Logement : ' . $reservation->annonce->title, 0, 1);
         $pdf->Cell(190, 10, 'Dates : Du ' . $reservation->date_debut . ' au ' . $reservation->date_fin, 0, 1);
         $pdf->Cell(190, 10, 'Prix total : ' . $reservation->prix_total . '€', 0, 1);
         $pdf->Ln(10);
 
         $pdf->SetFont('Arial', 'B', 12);
-        $pdf->Cell(190, 10, 'Coordonnées du Propriétaire', 0, 1);
+        $pdf->Cell(190, 10, 'Coordonnees du Proprietaire', 0, 1);
         $pdf->SetFont('Arial', '', 12);
         // $pdf->Cell(190, 10, 'Nom : ' . $reservation->proprietaire->name, 0, 1);
         // $pdf->Cell(190, 10, 'Email : ' . $reservation->proprietaire->email, 0, 1);
